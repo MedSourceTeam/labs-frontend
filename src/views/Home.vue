199728827b1445e0a347322a86042cb4c34f92c8
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-12-08 20:13:16 -0500

    Corregido agregar rol

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,51 @@
     <h1>Bienvenido al dummy</h1>
     <div class="row">
       <router-view></router-view>
+      <div v-if="isTeacher">Soy profesor</div>
     </div>
   </div>
 </template>
 
 <script>
-  import {getAuthenticationToken} from '@/dataStorage';
+import { getAuthenticationToken } from "@/dataStorage";
+import axios from "axios";
 
-  export default{
-    name: "Home",
-    beforeCreate( ){
-      if( !getAuthenticationToken( ) ){
-        this.$router.push( {name: 'login'} )
-      }
+export default {
+  name: "Home",
+
+  data: function() {
+    return {
+      isTeacher: false,
+    };
+  },
+
+  beforeCreate() {
+    if (!getAuthenticationToken()) {
+      this.$router.push({ name: "login" });
     }
-  }
+
+    axios
+      .get(this.$store.state.backURL + "/mis-roles", {
+        params: { access_token: getAuthenticationToken() },
+      })
+      .then((response) => {
+        if (response.status !== 200) {
+          alert("Error Obteniendo sus roles");
+        } else {
+          for (let r of response.data) {
+            if (r.id === 2) {
+              this.isTeacher = true;
+              break;
+            }
+          }
+        }
+      })
+      .catch((error) => {
+        alert("Error en la petición");
+        console.log(error);
+      });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
